aefb951a1e8af304542eaf75c727af3c54cb196b
AndresVelaF <Andres.Vela@fundacion-jala.org>
2023-06-14 22:34:51 -0500

    clase_3

--- game/components/enemies/enemy_handler.py
+++ game/components/enemies/enemy_handler.py
@@ -4,13 +4,22 @@ from game.components.enemies.ship_2 import Ship_2
 class EnemyHandler:
     def __init__(self):
         self.enemies = []
-        self.enemies.append(Ship())
-        self.enemies.append(Ship_2())
 
-    def update(self):
+    def update(self, bullet_handler):
+        self.add_enemy()
         for enemy in self.enemies:
-            enemy.update()
+            enemy.update(bullet_handler)
+            if not enemy.is_alive :
+                self.remove_enemy(enemy)
 
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
+
+    def add_enemy(self):
+        if len(self.enemies) < 2:
+            self.enemies.append(Ship())
+            self.enemies.append(Ship_2())
+
+    def remove_enemy(self, enemy):
+        self.enemies.remove(enemy)
